a10346f069d3af42ef467e1d023639186ec940a9
Nelson <nelsontko@gmail.com>
2016-02-04 19:58:20 +0000

    Started the read / write to template classes

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ This is a small application built around the AutoHotKey scripts that I designed 
 
 My eventual plan is to flesh it out by either superceding AutoHotKey, or creating templates
 for emails & functions that AutoHotKey uses.
+
+I intend to make this program modular and easy to edit the templates for the end user. More details
+to follow when it's more fleshed out.
--- src/automateyourjob/AltKey.java
+++ src/automateyourjob/AltKey.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Copyright (C) 2016 ntko
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package automateyourjob;
 import java.awt.event.KeyEvent;
@@ -22,12 +33,14 @@ public class AltKey {
         r.keyPress(KeyEvent.VK_2);
         // AutoHotKey needs to switch the tab to ensure that if the hotkey
         // process(es) are multi-threaded it prioritises them in the correct order
+        // eventually I will cycle out the use of AutoHotKey but in a manner
+        // that doesn't affect the end user. i.e. me and my colleagues.
         r.delay(10); //set delay or it won't work.
         r.keyRelease(KeyEvent.VK_ALT);
-        r.keyRelease(KeyEvent.VK_2); // ALT 2 to activate the window
+        r.keyRelease(KeyEvent.VK_2); // ALT 2 to activate the window with AHK
         r.keyPress(KeyEvent.VK_ALT);
         r.keyPress(keyCode);
-        r.delay(20); //set the delay
+        r.delay(20);
         r.keyRelease(KeyEvent.VK_ALT);
         r.keyRelease(keyCode); // ALT keyCode to start custom command
    
