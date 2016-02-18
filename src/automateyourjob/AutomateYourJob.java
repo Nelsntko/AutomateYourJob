596b6d6c88097522b25ab36106b3ce4d1e578750
Nelson <nelsontko@gmail.com>
2016-02-18 18:14:43 +0000

    Added default and load buttons to employment ref

--- src/automateyourjob/AutomateYourJob.java
+++ src/automateyourjob/AutomateYourJob.java
@@ -15,7 +15,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package automateyourjob;
-import automateyourjob.templates.AutomateYourJobUI;
 import java.awt.AWTException;
 /**
  *
--- src/automateyourjob/ReadTemplate.java
+++ src/automateyourjob/ReadTemplate.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package automateyourjob.templates;
+package automateyourjob;
 
 import com.google.common.base.Charsets;
 import com.google.common.io.Resources;
